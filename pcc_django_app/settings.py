4f5dd2ea7a2c324528fdc571fd05a4184977840e
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-03-29 20:22:44 +0900

    エラー修正用の行追加

--- pcc_django_app/settings.py
+++ pcc_django_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
@@ -19,6 +20,13 @@ LOGOUT_REDIRECT_URL = 'index'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
