9596b99e5d7069839c34a0ae3bd8d01cc55813ad
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-03-31 17:34:37 +0900

    レイアウトの変更・修正

--- pcc_django_app/settings.py
+++ pcc_django_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # ログインが必要なページに認証していないユーザがアクセスした場合のURL
 LOGIN_URL = 'login'
@@ -21,6 +22,9 @@ LOGOUT_REDIRECT_URL = 'index'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- pcc_home/views.py
+++ pcc_home/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
 def index(request):
+    contents = {
+        "name" : "Programming",
+        "desc" : "準備中…",
+    }
+    other = {
+        "name" : "Other",
+        "desc" : "準備中…",
+    }
+    news = {
+        "name" : "News",
+        "desc" : "準備中…",
+    }
+
+    lst = [contents, other, news]
+
     params = {
         'title' : 'PCC Office',
+        'content_list' : lst,
     }
     return render(request, 'pcc_home/index.html', params)
