050c22aafabf591b022f2958326610f06124f2a7
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-03-31 17:39:28 +0900

    staticファイル用の修正

--- pcc_django_app/settings.py
+++ pcc_django_app/settings.py
@@ -25,6 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
