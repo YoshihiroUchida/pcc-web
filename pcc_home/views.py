3ce82524cd662d1a1bb962f128b47cfb2d99b259
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-04-02 00:46:42 +0900

    コンテンツの追加操作を修正

--- pcc_home/views.py
+++ pcc_home/views.py
@@ -10,6 +10,15 @@ def Set_contents(n, d, u):
     return data
 
 #--------------------------------------------------
+def Set_pgm_contents(num, title, url):
+    data = {
+        'number' : str(num),
+        'sub_title' : title,
+        'url' : url,
+    }
+    return data
+
+#--------------------------------------------------
 def index(request):
     prog = Set_contents("Programming", "Pythonによるプログラミング", "program")
     other = Set_contents("Other", "準備中", "#")
@@ -24,6 +33,16 @@ def index(request):
     return render(request, 'pcc_home/index.html', params)
 
 #--------------------------------------------------
+# ログイン後のみ利用可
 @login_required
 def program(request):
-    return render(request, 'pcc_home/page1.html', {})
+    lst = []
+    lst.append(Set_pgm_contents(1, "プログラミングとは?", "https://docs.google.com/presentation/d/e/2PACX-1vR_aklF0GAAx6mPr4eUXhtUS-nZNcu4jwqqdKyZbkxD1QvKQzfLSDzYizcyGXNFyJ4QGXNyaHDiK-Df/embed?start=false&loop=false&delayms=3000"))
+    lst.append(Set_pgm_contents(2, "Spyderの使い方", "#"))
+    lst.append(Set_pgm_contents(3, "変数の使い方", "#"))
+    
+    params = {
+        'title' : 'Programming',
+        'tab_list' : lst,
+    }
+    return render(request, 'pcc_home/page1.html', params)
