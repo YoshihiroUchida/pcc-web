8a4afda74d5ba80656ae1c967fcdb95717d0106d
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-04-04 17:14:05 +0900

    URLの更新

--- pcc_home/views.py
+++ pcc_home/views.py
@@ -37,9 +37,9 @@ def index(request):
 @login_required
 def program(request):
     lst = []
-    lst.append(Set_pgm_contents(1, "プログラミングとは?", "https://docs.google.com/presentation/d/e/2PACX-1vR_aklF0GAAx6mPr4eUXhtUS-nZNcu4jwqqdKyZbkxD1QvKQzfLSDzYizcyGXNFyJ4QGXNyaHDiK-Df/embed?start=false&loop=false&delayms=3000"))
-    lst.append(Set_pgm_contents(2, "Spyderの使い方", "#"))
-    lst.append(Set_pgm_contents(3, "変数の使い方", "#"))
+    lst.append(Set_pgm_contents(1, "プログラミングとは?", "https://docs.google.com/presentation/d/e/2PACX-1vRCR6-FsFsvyewZcUw4tPBNpJZHFe-cjHWEky_2rCbmETFsrGcdU6r1h32XmcvSf_ttckcMjjh-tPuu/embed?start=false&loop=false&delayms=3000"))
+    lst.append(Set_pgm_contents(2, "Spyderのつかいかた", "https://docs.google.com/presentation/d/e/2PACX-1vRWiINR95pAuelWGSUt4NKkI7ZIZOH9wQtHy19llGSNDvep3vZAmwfmO6aLn-TuCCzP-Xwo3iu_PCxF/embed?start=false&loop=false&delayms=3000"))
+    lst.append(Set_pgm_contents(3, "変数のつかいかた", "https://docs.google.com/presentation/d/e/2PACX-1vShsE3EHzSRo-Pm5Ezc_Y7V9tRA47_RovfLp9M-Jet4c4csoUjJrUh0ONUJMyh82OQETANqAIUrAdrc/embed?start=false&loop=false&delayms=3000"))
     
     params = {
         'title' : 'Programming',
