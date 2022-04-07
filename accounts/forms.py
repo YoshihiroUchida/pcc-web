ec3110eca0adaeb5dec045f2495d495b8cbfd39f
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-04-07 13:33:30 +0900

    UIの修正・変更

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.forms import AuthenticationForm 
 
-# ログイン用 (ユーザ名／パスワード)
+# ログイン用 (ユーザー名／パスワード)
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.label_suffix = " "
+
         for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control' # class属性へ指定
-            field.widget.attrs['placeholder'] = field.label # label属性の指定
+            field.widget.attrs['class'] = 'form-control'
--- pcc_home/forms.py
+++ pcc_home/forms.py
@@ -3,17 +3,32 @@ from .models import Prog_contents
 
 #--------------------------------------------------
 class Prog_contents_form(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = " "
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Prog_contents
         fields = ['num', 'title', 'web_site']
+        labels = {
+            'num'       : '番号',
+            'title'     : 'タイトル',
+            'web_site'  : 'URL',
+        }
 
 #--------------------------------------------------
 class Prog_form(forms.Form):
-    num = forms.IntegerField(label = 'num', \
-        widget = forms.NumberInput(attrs = {'class' : 'form-control'}))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = " "
 
-    title = forms.CharField(label = 'title', \
+    num = forms.IntegerField(label = '番号', \
+        widget = forms.NumberInput(attrs = {'class' : 'form-control', 'placeholder': 0}))
+
+    title = forms.CharField(label = 'タイトル', \
         widget = forms.TextInput(attrs = {'class' : 'form-control'}))
 
-    web_site = forms.URLField(label = 'web_site', \
+    web_site = forms.URLField(label = 'URL', \
         widget = forms.TextInput(attrs = {'class' : 'form-control'}))
